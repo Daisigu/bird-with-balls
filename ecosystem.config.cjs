74de046653e21057e0c1dff005ed7f1b803370ae
Богдан Гоголь <bogd2288@gmail.com>
2024-12-07 16:52:26 +0300

    refactor: ecosystem.config.cjs

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -5,7 +5,15 @@ module.exports = {
       port: "3002",
       exec_mode: "cluster",
       instances: "max",
+      watch: true,
+      autorestart: true,
+      env: {
+        NODE_ENV: "production",
+      },
+      max_restarts: 5,
+      restart_delay: 5000,
       script: "./.output/server/index.mjs",
     },
+
   ],
 };
