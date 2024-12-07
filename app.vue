b3b5e0f1cee2e833f8805e22a24845cfb118fa01
Богдан Гоголь <bogd2288@gmail.com>
2024-12-07 16:31:32 +0300

    refactor: seo + components

--- app.vue
+++ app.vue
@@ -1,11 +1,35 @@
+<script setup lang="ts">
+useHead({
+  htmlAttrs: {
+    lang: 'en'
+  },
+  link: [
+    {
+      rel: 'icon',
+      type: 'image/svg+xml',
+      href: '/favicon.svg',
+    }
+  ]
+})
+
+useSeoMeta({
+  title: 'Spinning bird with Balls',
+  description: 'Spinning bird with Balls',
+  ogTitle: 'Spinning Bird with Balls',
+  ogDescription: 'Spinning Bird with Balls',
+  ogImage: '/main-image.jpg',
+  ogUrl: 'https://example.com',
+  twitterTitle: 'Spinning Bird with Balls',
+  twitterDescription: 'Spinning Bird with Balls',
+  twitterImage: '/main-image.jpg',
+  twitterCard: 'summary_large_image'
+})
+
+</script>
 <template>
   <div class="flex-grow text-white bg-black flex-col flex min-h-screen">
-    <div>
-      123
-    </div>
+    <VHeader/>
     <VBirdModel class="flex-grow"/>
-    <div>
-      123
-    </div>
+    <VFooter/>
   </div>
 </template>
