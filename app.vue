b531d8b57375fc2d1639586febd2ae05f82db424
Богдан Гоголь <bogd2288@gmail.com>
2024-12-07 15:08:46 +0300

    Initial commit 🐦

---
+++ app.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex-grow text-white bg-black flex-col flex min-h-screen">
+    <div>
+      123
+    </div>
+    <VBirdModel class="flex-grow"/>
+    <div>
+      123
+    </div>
+  </div>
+</template>
---
+++ components/VBirdModel.client.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+const {scene} = await useGLTF('/models/bird.gltf', {draco: true})
+</script>
+
+<template>
+  <div class="bg-black">
+    <TresCanvas alpha>
+      <!-- Камера -->
+      <TresPerspectiveCamera :position="[1, 2, 3]"/>
+      <OrbitControls
+          auto-rotate
+          :enable-rotate="true"
+          :enable-zoom="true"
+          :enable-damping="true"
+          :enable-pan="false"
+          :auto-rotate-speed="10"
+          :min-polar-angle="0"
+          :max-polar-angle="3.14"
+      />
+      <!-- Основной направленный свет -->
+      <TresDirectionalLight :position="[5, 10, 5]" :intensity="2" />
+      <TresDirectionalLight :position="[-5, -10, -5]" :intensity="2" />
+      <TresDirectionalLight :position="[0, -10, 40]" :intensity="2" />
+      <TresDirectionalLight :position="[0, 5, -5]" :intensity="2" />
+      <!-- Отображение модели -->
+      <primitive :object="scene"/>
+    </TresCanvas>
+  </div>
+</template>
