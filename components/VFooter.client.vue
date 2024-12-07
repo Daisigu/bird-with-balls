7e5bbb296eb908baa6709f59f3dad0264455ab2b
Богдан Гоголь <bogd2288@gmail.com>
2024-12-07 16:30:39 +0300

    feat: VFooter component

---
+++ components/VFooter.client.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const protocol = location.protocol === 'https:' ? 'wss' : 'ws'
+const { data } = useWebSocket(`${protocol}://${location.host}/api/websocket`)
+const usersWatching = useState('usersWatching', () => 1)
+
+watch(data, (newValue) => {
+  if (newValue) {
+    try {
+      const parsed = JSON.parse(newValue)
+      usersWatching.value = parsed.userCount || 1  // Обновляем с правильным значением
+    } catch (error) {
+      console.error('Failed to parse WebSocket message', error)
+    }
+  }
+})
+</script>
+
+<template>
+  <footer class="p-2">
+    <span class="font-mono text-xs sm:text-lg">Users watching spinning bird with balls: {{ usersWatching }}</span>
+  </footer>
+</template>
